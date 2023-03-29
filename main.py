0a4d7dbe889d44d963095d6fcd54352152b0b144
dldavros <smailde0307@gmail.com>
2023-03-29 15:16:57 +0700

    Test main

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import Functions
+
+admin_list = []
+staff_list = []
+book_list = []
+customers_list = []
+
 print(f"What are you? \n"
       f"1. Admin \n"
       f"2. Staff \n")
-op = int(input(f"Your choice: "))
-match op:
+op1 = int(input(f"Your choice: "))
+match op1:
     case 1:
-        print(f"What do you want? \n")
+        print(f"Ok Assmin, what do you want? \n")
         
     case 2:
-        print(f"What do you want? \n")
+        print(f"Ok staff, what do you want? \n")
         
     case _:
         print(f"Invalid choice!")
